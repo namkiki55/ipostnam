329c736d3129b307fd145e9ad8ea1e281ea7f812
namkiki55 <namkiki55@gmail.com>
2021-01-23 23:58:11 +0700

    my first git commit

---
+++ includes/content-footer.php
@@ -0,0 +1,8 @@
+<!-- <footer class="main-footer">
+    ระบบบริหารจัดการวิทยานิพนธ์ - <strong>Powered by CS61</strong>
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 1.0
+    </div>
+</footer> -->
+<aside class="control-sidebar control-sidebar-dark">
+</aside>
